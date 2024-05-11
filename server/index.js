dd33c9432c1247be5fdbdeef0d5b787c1b9e0aae
Darius <kudoreha9@gmail.com>
2024-05-11 17:33:12 -0400

    Installed Express, used path module from Node.js, fixed unrelencoded method, and added dist file path into express.static()

--- server/index.js
+++ server/index.js
@@ -3,6 +3,14 @@
 // Create variable to hold it
 const express = require('express');
 
+
+// Provides utilities for working with file and directory paths
+const path = require('path'); // Make sure to have node.js installed
+// Using resolve method
+// Resolves a given value to a Promise
+
+const distPath = path.resolve(__dirname, '..', 'dist');
+
 // Create an instance of an Express application
 const app = express(); // => A function provided by the Express.js framework
 // App object represents web application
@@ -10,8 +18,9 @@ const app = express(); // => A function provided by the Express.js framework
 
 // Setting up middleware
 app.use(express.json()); // Parse the request body
-app.use(express.urlencoded()); // Parses url
-app.use(express.static()); // Statically serve up client directory
+app.use(express.urlencoded({ extended: true })); // Parses url
+app.use(express.static(distPath)); // Statically serve up client directory
+// Adding dist file path into express.static()
 
 // Setting environment variable
 // The port express will listen on
