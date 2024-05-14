de6c1dafb6aa78addddd24942a417e954c70b868
cody-daigle <cody.h.daigle@gmail.com>
2024-05-14 14:21:39 -0500

    Add layout for routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -8,3 +8,19 @@ const user = require('./user');
 const router = Router();
 
 // Add all routes here
+
+//homepage routes for feed
+// router.route('/homepage').get(observations.feed);
+// router.route('/homepage').patch(observations.reaction);
+
+// Wildlife routes
+
+// Plants routes
+
+// Hikes routes
+
+// user/profile routes
+
+module.exports = {
+  router,
+};
