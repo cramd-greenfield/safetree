6b679ec09ba3b8c86ece38d2195a949d1bced04a
mike <sammartino.mike@gmail.com>
2024-05-13 20:45:11 -0400

    add components for hike search results

---
+++ client/components/HikeResult.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const HikeResult = () => {
+
+  return (
+    <div>
+      <span>Hike Description</span>
+      <span>Hike Location</span>
+      <button>Add Favorite</button>
+    </div>
+  )
+}
+
+export default HikeResult;
