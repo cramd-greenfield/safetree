24b0504d798a0a8c8dd9cdee76693e38d7fc6f91
Allyn McKinney <allynmckinney@Allyns-MacBook-Pro.local>
2024-05-14 20:01:33 -0500

    successful req to api with search

--- server/routes/plants.js
+++ server/routes/plants.js
@@ -4,27 +4,35 @@ const { Plant } = require('../database/index.js');
 
 const Plants = Router();
 
-console.log(searchPlants);
-
 // makes search to api
 Plants.get('/plants', (req, res) => {
   // would this be for the search ?
+
+  // access search value from params (?)
+  const { plantName } = req.params;
+
   // make req to api
-  // save results of search to database
+  searchPlants(plantName)
+    .then(({data} => console.log(data))) // respond with list of plants + 200 status code
+    .catch(err => console.error(err)); // respond w 500 status code
 });
 
 
 Plants.post('/plants', (req, res) => {
-  
-
+  // save plant to db
+  // access plant obj from req.body
+  const { plantObj } = req.body;
+  // pass plantObj to Plant.create
+  Plant.create()
+    .then()
 });
 
-Plants.update('/plants', (req, res) => {
-
+Plants.patch('/plants/:id', (req, res) => {
+  // update isEdible property
 });
 
 Plants.delete('/plants', (req, res) => {
-
+  // delete plant obj
 });
 
 module.exports = {
