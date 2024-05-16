10828a4eed2045b99c63c9df22cb92a9ebd15add
cody-daigle <cody.h.daigle@gmail.com>
2024-05-16 11:50:00 -0500

    Resolved import error for logo image

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../../style/logo.png';
+import logo from '../style/logo.png';
 
 const Home = () => {
   return (
