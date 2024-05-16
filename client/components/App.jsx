7b3240dc79144af07d5feecb05b55645046059f8
cody-daigle <cody.h.daigle@gmail.com>
2024-05-16 11:41:42 -0500

    Add profile route to app page

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import Home from './observations/Home.jsx';
 import Login from './observations/Login.jsx';
+import Profile from './Profile.jsx';
 import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
     <>
       <Routes>
-        <Route path='/home' element={<Home />} />
         <Route path='/' element={<Login />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/profile' element={<Profile />} />
       </Routes>
     </>
   );
