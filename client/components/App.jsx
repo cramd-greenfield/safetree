38b91a91f3745ffa5c0f7045cef34fd7a10beb7a
cody-daigle <cody.h.daigle@gmail.com>
2024-05-16 03:50:35 -0500

    Routes to home page after login

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,55 +1,16 @@
 import React from 'react';
 import Home from './observations/Home.jsx';
 import Login from './observations/Login.jsx';
-
-// import Calendar from './Calendar.jsx';
-import WildLife from './WildLife.jsx';
-import Plants from './plants/Plants.jsx';
-import Hikes from './hikes/Hikes.jsx';
+import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div>
-      <div className='features'>
-        <div className='feature'>
-          <div className='wildlife'>
-            <h4>wildlife component here</h4>
-            <WildLife />
-          </div>
-        </div>
-        <div className='feature'>
-          <div className='plants'>
-            <Plants />
-          </div>
-        </div>
-        <div className='feature'>
-          <div className='activities'>
-            <h4>Hikes</h4>
-            <Hikes />
-          </div>
-        </div>
-        <div className='feature'>
-          <div className='calendar'>
-            <h4>
-              calendar component here
-              {/* <Calendar /> */}
-            </h4>
-          </div>
-        </div>
-        <div className='feature'>
-          <div className='home-observations'>
-            <h4>observations component here</h4>
-            <>
-              <Login />
-              <Home />
-            </>
-          </div>
-        </div>
-      </div>
-      <div className='footer'>
-        <p>bottom of page things go here</p>
-      </div>
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/home' element={<Home />} />
+      </Routes>
+    </>
   );
 };
 
