e35a61e471c59a5f9f1a84476a1e7781bace1eca
mike <sammartino.mike@gmail.com>
2024-05-16 18:31:13 -0400

    set up POST request to external api, may not keep

--- client/components/hikes/HikeSearch.jsx
+++ client/components/hikes/HikeSearch.jsx
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+// require('dotenv').config();
+// const { GOOGLE_MAPS_API_KEY } = process.env;
 
 const HikeSearch = () => {
 
+  const [input, setInput] = useState('');
+
+  const handleInput = (e) => {
+    setInput(e.target.value);
+    console.log(e.target.value);
+  }
+
+  const searchHikes = () => {
+    console.log('hike searched');
+    console.log(input);
+
+    axios.post('https://places.googleapis.com/v1/places:searchText', {
+      params: {
+        fields: 'places.id,places.displayName,places.location,places.primaryTypeDisplayName,places.rating,places.formattedAddress',
+        textQuery: `hikes near ${input}`,
+        key: GOOGLE_MAPS_API_KEY,
+        maxResultCount: 5,
+        rankPreference: 'DISTANCE',
+      }
+    })
+      .then((data) => {
+        console.log('results from google: ', data);
+      })
+      .catch((err) => {
+        console.error('Failed to fetch results from Google Maps', err);
+      })
+  }
+
   return (
     <div className="hike-search">
-      <input type="text" />
-      <button>Search</button>
+      <input value={ input } onChange={ handleInput } type="text" placeholder="city, state, or zip code"/>
+      <button onClick={ searchHikes }>Search</button>
     </div>
   )
 }
