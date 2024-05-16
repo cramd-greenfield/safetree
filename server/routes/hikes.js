f694393cd30a0e9b39b1d3e978a530cf2209ebec
mike <sammartino.mike@gmail.com>
2024-05-16 11:18:02 -0400

    POST request to /hikes working

--- server/routes/hikes.js
+++ server/routes/hikes.js
@@ -2,25 +2,15 @@ const express = require('express');
 const hikes = express.Router();
 const { Hike } = require('../database');
 
-// in app.js, there will be something like app.use('/', routerIndex),
-// and then in server/routes/index.js, router.use('/hikes', hikes);
-// so all requests starting with /hikes will come here, and these
-// endpoints will handle requests to endpoints '/hikes/*'
-
 // routes for hike related requests
 
 hikes.post('/hikes', (req, res) => {
-  /**
-   * description
-   * location
-   * rating
-   */
+
   const { description, location, rating } = req.body.hike;
 
   // add hike to the database
   Hike.create({ description, location, rating })
-    .then((data) => {
-      console.log('Added favorite hike: ', data);
+    .then(() => {
       res.sendStatus(201);
     })
     .catch((err) => {
@@ -44,7 +34,7 @@ hikes.get('/hikes', (req, res) => {
 
 });
 
-hikes.put('/hikes', (req, res) => {
+hikes.patch('/hikes', (req, res) => {
   // update certain fav hike rating in db
 
   // use Hike sequelize model.update() to update rating
