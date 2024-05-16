a74c65ad3ab7bf2ab85c88043a5f4f9ced1237a9
Allyn McKinney <allynmckinney@Allyns-MacBook-Pro.local>
2024-05-16 14:49:43 -0500

    create plant list component

---
+++ client/components/plants/PlantList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const PlantList = () => {
+  return (
+    <ul>
+      <li>Plant</li>
+      <li>Plant</li>
+      <li>Plant</li>
+    </ul>
+  );
+};
+
+export default PlantList;
