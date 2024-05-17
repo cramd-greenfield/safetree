8d32f296ef7987f1806f669a0cc18633926e392b
mike <sammartino.mike@gmail.com>
2024-05-17 15:16:59 -0400

    relocate HikeFavList and HikeResults as children components to HikeSearch

--- client/components/hikes/Hikes.jsx
+++ client/components/hikes/Hikes.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 
 import HikeSearch from './HikeSearch.jsx';
-// import HikeResults from './HikeResults.jsx';
-// import HikeFavList from './HikeFavList.jsx';
 
 const Hikes = () => {
 
@@ -13,14 +11,6 @@ const Hikes = () => {
         <h4>Search for trails near you:</h4>
         <HikeSearch />
       </div>
-      {/* <div className="hike-results">
-        <h4>Local Results</h4>
-        <HikeResults results={ results } />
-      </div> */}
-      {/* <div className="hike-favs">
-        <h4>Favorite Trails</h4>
-        <HikeFavList />
-      </div> */}
     </div>
   )
 }
