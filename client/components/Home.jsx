70c83e5a1674501ae55026d6b84a110c73c0bacf
cody-daigle <cody.h.daigle@gmail.com>
2024-05-19 15:17:08 -0500

    Feat / Added open and close feature to home

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -9,37 +9,34 @@ import {
   Checkbox,
   Avatar,
   TextField,
+  Divider,
+  Dialog,
 } from '@mui/material';
 import logo from '../style/logo.png';
 import ObservationsList from './observations/ObservationsList.jsx';
-// import Custom from './observations/Custom.jsx';
+import ObsForm from './observations/ObsForm.jsx';
+import { st, reviewBox, boxHeader } from './observations/styles.js';
 
 const Home = () => {
   const [observations, setObservations] = useState([]);
-  const [message, setMessage] = useState('');
+  const [open, setOpen] = useState(false);
   // const [isSafe, setIsSafe] = useState(false);
   const obRef = useRef(observations);
 
-  const createObservation = () => {
-    axios
-      .post(`/observations`, { observation: { message, isSafe } })
-      .then(() => {
-        getObservations();
-      })
-      .then(() => {
-        setMessage('');
-      })
-      .catch((err) => {
-        console.error('Failed to Create Observation:', err);
-      });
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
   };
 
   const getObservations = () => {
     axios
       .get(`/observations`)
       .then(({ data }) => {
-        // setObservations(data);
-        setObservations((...prevData) => (prevData = data));
+        setObservations(data);
+        // setObservations((...prevData) => (prevData = data));
       })
       .catch((err) => console.error('Could not get Feed:', err));
   };
@@ -49,16 +46,7 @@ const Home = () => {
 
   return (
     <div>
-      <Box
-        sx={{
-          backgroundImage: `url(${logo})`,
-          backgroundRepeat: 'no-repeat',
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-          minHeight: 200,
-          padding: 10,
-        }}
-      >
+      <Box sx={st}>
         <Box>
           <Typography variant='h2' align='center' gutterBottom>
             SafeTree
@@ -68,44 +56,40 @@ const Home = () => {
         <Box>
           <Avatar src={`${logo}`} />
         </Box>
-        <Box
-          height={400}
-          width={500}
-          sx={{
-            border: '4px solid black',
-            backgroundColor: 'background.paper',
-          }}
-        >
-          <Box
-            sx={{
-              gap: 20,
-              display: 'inline-flex',
-              backgroundColor: 'green',
-              width: '100%',
+        <Box height={300} width={400} sx={reviewBox}>
+          <Box sx={boxHeader}>
+            <Typography align='left' variant='h8'>
+              Review
+            </Typography>
+            <Typography align='right' variant='h8'>
+              Safe: / Unsafe:
+            </Typography>
+          </Box>
+          <Box sx={{ overflow: 'hidden' }}>
+            <ObservationsList
+              observations={observations}
+              getObservations={getObservations}
+            />
+          </Box>
+          <Dialog
+            open={open}
+            onClose={handleClose}
+            PaperProps={{
+              component: 'form',
+              onSubmit: (e) => {
+                handleClose(e.target.value);
+              },
             }}
           >
-            <Typography align='left' variant='h8'>
-              Remove
-            </Typography>
-
-            <Typography variant='h8'>Review</Typography>
-          </Box>
-
-          <ObservationsList
-            observations={observations}
-            getObservations={getObservations}
-          />
+            <ObsForm
+              getObservations={getObservations}
+              handleClose={handleClose}
+            />
+          </Dialog>
         </Box>
-        <Box
-          component='form'
-          sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}
-        ></Box>
-        <Button variant='contained' type='submit' onClick={createObservation}>
-          Submit
+        <Button variant='contained' type='submit' onClick={handleOpen}>
+          Edit Reviews
         </Button>
-        {/* <Button type='submit' onClick={updateSafe} variant='contained'>
-          Update Review
-        </Button> */}
       </Box>
     </div>
   );
