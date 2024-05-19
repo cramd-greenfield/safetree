eec6a86fe4aa06c5778834571d3d60258299142d
cody-daigle <cody.h.daigle@gmail.com>
2024-05-19 14:30:28 -0500

    Feat / Added style file to declutter styling

---
+++ client/components/observations/styles.js
@@ -0,0 +1,17 @@
+import logo from '../../style/logo.png';
+
+const st = {
+  backgroundImage: `url(${logo})`,
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: 'cover',
+  backgroundPosition: 'center',
+  minHeight: 200,
+  padding: 10,
+};
+
+const reviewBox = {
+  border: '4px solid black',
+  backgroundColor: 'background.paper',
+};
+
+export { st, reviewBox };
