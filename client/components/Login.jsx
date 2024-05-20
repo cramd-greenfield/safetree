59aa3631aa0648c364f0d489a8ec4d68a55753e2
cody-daigle <cody.h.daigle@gmail.com>
2024-05-19 23:43:57 -0500

    Login / Updated login slightly

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
-import { Button } from '@mui/material';
+import { Button, Box, Typography } from '@mui/material';
+import { st } from '../components/observations/styles.js';
 
 const Login = () => {
   return (
     <div>
-      <h3>Login Screen</h3>
-      <form action='/auth/google' method='GET'>
-        <Button variant='contained' type='submit'>
-          Sign-In
-        </Button>
-      </form>
+      <Box sx={st}>
+        <Box>
+          <Typography variant='h2' align='center' gutterBottom>
+            SafeTree
+          </Typography>
+        </Box>
+        <form action='/auth/google' method='GET'>
+          <h3>Login to Safe-Tree Here!</h3>
+          <Button variant='contained' type='submit'>
+            Sign-In
+          </Button>
+        </form>
+      </Box>
     </div>
   );
 };
