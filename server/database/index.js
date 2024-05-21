27db6e5666d4fbc7471e15f7a55661b0c8735b58
uhrinuh <nasri.arina@gmail.com>
2024-05-21 14:41:12 -0700

    created 2 new models: PackingList and PackingListItem

--- server/database/index.js
+++ server/database/index.js
@@ -80,6 +80,68 @@ const Observations = sequelize.define('observation', {
   message: DataTypes.TEXT,
   isSafe: DataTypes.BOOLEAN,
 });
+
+// define a sequelize model called PackingList
+const PackingList = sequelize.define('PackingList', {
+  // define id column of PackingList table
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  // define userId column of PackingList table
+  // it is a foreign key aka references the id column of the User table
+  userId: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: User,
+      key: 'id',
+    },
+  },
+});
+
+// define a sequelize model called PackingListItem
+const PackingListItem = sequelize.define('PackingListItem', {
+  // define the id column of the PackingListItem
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  // columns in the table
+  name: DataTypes.STRING,
+  quantity: DataTypes.INTEGER,
+  packed: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+  // define packingListId column of PackingListItem table
+  // it is a foreign key aka references the id column of the PackingList table
+  packingListId: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: PackingList,
+      key: 'id',
+    },
+  },
+});
+
+// 1:1 relationship between User and PackingList
+// aka 1 User has 1 PackingList
+// add foreignKey to specify which column in PackingList references User table
+User.hasOne(PackingList, { foreignKey: 'userId' });
+// PackingList belongs to user
+// again adds foreignKey to specify which column in PackingList references User table
+PackingList.belongsTo(User, { foreignKey: 'userId' });
+
+// 1:N relationship between PackingList and PackingListItem
+// aka PackingList can have many PackingListItems
+// add foreignKey to specify which column in PackingListItem references PackingList table
+PackingList.hasMany(PackingListItem, { foreignKey: 'packingListId' });
+// each PackingListItem belongs to a PackingList
+// again adds foreignKey to specify which column in PackingListItem references PackingList table
+PackingListItem.belongsTo(PackingList, { foreignKey: 'packingListId' });
+
 User.Observations = User.hasMany(Observations);
 Observations.User = Observations.belongsTo(User);
 
@@ -90,5 +152,7 @@ module.exports = {
   Itinerary,
   Hike,
   Observations,
+  PackingList,
+  PackingListItem,
   sequelize,
 };
